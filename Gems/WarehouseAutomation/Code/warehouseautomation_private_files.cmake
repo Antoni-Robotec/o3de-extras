d077414a1cc3eac7e1e704a0521d63106765fb2f
Antoni Puch <antoni.puch@robotec.ai>
2023-07-27 15:02:39 +0200

    Test commit
    Signed-off-by: Antoni Puch <antoni.puch@robotec.ai>

---
+++ Gems/WarehouseAutomation/Code/warehouseautomation_private_files.cmake
@@ -0,0 +1,16 @@
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+set(FILES
+    Source/WarehouseAutomationModuleInterface.cpp
+    Source/WarehouseAutomationModuleInterface.h
+    Source/Clients/WarehouseAutomationSystemComponent.cpp
+    Source/Clients/WarehouseAutomationSystemComponent.h
+    Source/ConveyorBelt/ConveyorBeltComponent.cpp
+    Source/ConveyorBelt/ConveyorBeltComponent.h
+    Source/ConveyorBelt/ConveyorBeltComponentConfiguration.cpp
+    Source/ConveyorBelt/ConveyorBeltComponentConfiguration.h
+    Source/ProximitySensor/ROS2ProximitySensor.cpp
+    Source/ProximitySensor/ROS2ProximitySensor.h
+)
